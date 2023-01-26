6d360d62c5a0432d2d298999d704aa0f79dbcfcc
Furkannkrts <45968068+Furkannkrts@users.noreply.github.com>
2023-01-26 15:26:21 +0300

    Update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World değişti!");
+
+{
+
+    Console.WriteLine("Bilgi Yarışmsına Hoşgeldiniz");
+    Console.WriteLine();
+    Console.WriteLine("---------------------------");
+    Console.WriteLine();
+
+    int soru = 1;
+    string cevap;
+    if (soru == 1)
+    {
+        Console.WriteLine("Kanada'nın Başkenti Neresidir?");
+        Console.WriteLine();
+        Console.WriteLine("A) Toronto");
+        Console.WriteLine("B) Ottowa");
+        Console.WriteLine("C) Montreal");
+        Console.WriteLine("D) Vancouver");
+        Console.WriteLine();
+        Console.Write("Cevabınız: ");
+        cevap=Console.ReadLine();
+
+        if(cevap=="b"||cevap=="B") 
+        {
+            Console.WriteLine("Cevabınız Doğru");
+        }
+        else 
+        {
+            Console.WriteLine("Cevabınız Yanlış");
+        }
+    }
+
+}
